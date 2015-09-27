d25ff998130fc3b31a87830c4223e9b5cd7f7f37
Chris Wong <chris@cogifire.com>
2015-09-26 23:22:38 -0700

    Distance

--- check/www/js/controllers.js
+++ check/www/js/controllers.js
@@ -4,7 +4,6 @@ angular.module('starter.controllers', [])
   // document.addEventListener("deviceready", function () {
   //   $cordovaPlugin.someFunction().then(success, error);
   // }, false);
-  //$scope.listing = [ {name: 'test'} ];
 
   var posOptions = {timeout: 10000, enableHighAccuracy: false};
   var location = $cordovaGeolocation.getCurrentPosition(posOptions);
@@ -15,25 +14,26 @@ angular.module('starter.controllers', [])
       console.log(latitude);
       console.log(longitude);
 
-      // JSON.parse('https://api.soleo.com/businesses?Category=Restaurants&Latitude=' + latitude + '&Longitude=' + longitude + '&Radius=10&APIKey=hnqee6js7rekm8txj7p6fqbw', function(k, v) {
-      //   console.log("test " + k);
-      //   return v;
-      // });
-
       var url = 'https://api.soleo.com/businesses?Category=Restaurants&Latitude=' + latitude + '&Longitude=' + longitude + '&Radius=20&APIKey=hnqee6js7rekm8txj7p6fqbw';
 
       $.getJSON(url,function(data) {
         $scope.listing = data.businesses; 
             for (i = 0; i < 7; i++) {
-              var list = document.getElementById('listing').innerHTML + '<div class="list card"><div class="item"><h2>' + $scope.listing[i].name + '</h2><p>' + $scope.listing[i].address + " " + $scope.listing[i].city + " " + $scope.listing[i].state + " " + $scope.listing[i].zip + '</p></div><a class="item item-icon-left assertive" href="#"><i class="icon ion-android-navigate"></i>Navigate</a>';
-              document.getElementById('listing').innerHTML = list;
-                // document.write('<a class="item item-icon-left assertive" href="#">');
-                //   document.write('<i class="icon ion-android-navigate"></i>');
-                //   document.write('Navigate');
-                // document.write('</a>');
-              }
+              var radlat1 = Math.PI * latitude/180;
+              var radlat2 = Math.PI * $scope.listing[i].latitude/180;
+              var radlon1 = Math.PI * longitude/180;
+              var radlon2 = Math.PI * $scope.listing[i].longitude/180;
+              var theta = longitude-$scope.listing[i].longitude;
+              var radtheta = Math.PI * theta/180;
+              var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+              dist = Math.acos(dist);
+              dist = dist * 180/Math.PI;
+              dist = dist * 60 * 1.1515;
+              dist = dist.toFixed(2);
 
-        //document.write($scope.listing[0].name);
+              var list = document.getElementById('listing').innerHTML + '<div class="list card"><div class="item"><h2>' + $scope.listing[i].name + '</h2><p>' + $scope.listing[i].address + " " + $scope.listing[i].city + " " + $scope.listing[i].state + " " + $scope.listing[i].zip + '</p></div><a class="item item-icon-left assertive" href="#"><i class="icon ion-android-navigate"></i>Navigate (' + dist + ' miles)</a>';
+              document.getElementById('listing').innerHTML = list;
+              }
       });
   }, function(err) {
     // error
