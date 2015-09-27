8320ee2b5393e9ac78943b3498b61bd006980a54
Chris Wong <chris@cogifire.com>
2015-09-27 12:57:49 -0700

    ListUI

--- reg/www/js/controllers.js
+++ reg/www/js/controllers.js
@@ -1,14 +1,99 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
-  $scope.addCount = function() {
-    var url = 'https://prod-mmx-001.magnet.com:5221/mmxmgmt/api/v1/topics';
-    var currentName;
-  };
+  // <!-- class -->
+  function Reservation(inName, inPartySize) {
+    // <!-- variables -->
+    this.username = inName;
+    this.partySize = inPartySize;
+    this.waitTime = 0;
+    this.posInQueue = 0;
+  }
+    
+
+    // <!-- constructor -->
+    Reservation.prototype = {
+      constructor: Reservation,
+      //this.username = inName;
+      //this.partySize = inPartySize;
+      //console.log("constructor called");
+    };
+    // <!-- functions -->
+    Reservation.prototype.getWaitTime = function (){
+      calculateWaitTime();
+      return this.waitTime;
+    };
+
+    Reservation.prototype.setPosInQueue = function (inPosInQueue){
+      this.posInQueue = inPosInQueue;
+    };
+
+    Reservation.prototype.calculateWaitTime = function () {
+      this.waitTime = (this.posInQueue)*10;
+    };
   
-  // $.post(url, data, function(response) {
-  //   console.log('POST request success');
-  // }, 'json');
+    // <!-- class -->
+    function WaitingListHandler() {}
+
+      WaitingListHandler.prototype.waitingListQueue = [];
+      WaitingListHandler.prototype.waitingListSearchKeys = [];
+      WaitingListHandler.prototype.numOfReservations = 0;
+
+      WaitingListHandler.prototype.updateQueuePositions = function (){
+        for(var i = 0; i < this.numOfReservations; i++) {
+          this.waitingListQueue[i].setPosInQueue(i+1);
+          this.waitingListQueue[i].calculateWaitTime();
+        }
+      };
+      WaitingListHandler.prototype.removeReservation = function (name){
+        var targetIndex = this.waitingListSearchKeys.indexOf(name);
+        var target = this.waitingListQueue[targetIndex];
+
+        // <!-- remove item from both arrays -->
+        this.waitingListQueue.splice(targetIndex, 1);
+        this.waitingListSearchKeys.splice(targetIndex, 1);
+
+        // <!-- decrement the number of reservations -->
+        this.numOfReservations--;
+        this.updateQueuePositions();
+        return target;
+      };
+      WaitingListHandler.prototype.addReservation = function (reservation){
+        this.waitingListQueue.push(reservation);
+        this.waitingListSearchKeys.push(reservation.username);
+        this.numOfReservations++;
+        this.updateQueuePositions();
+      };
+  
+  $scope.reservationHandler = new WaitingListHandler();
+
+  $scope.addCount = function(inPartyName, inPartySize) {
+    //console.log(inPartyName + ' ' + inPartySize);
+
+    $scope.reservationHandler.addReservation(new Reservation(inPartyName, inPartySize));
+    // var link = 'https://prod-mmx-001.magnet.com:5221/mmxmgmt/api/v1/topics?topicName=test';
+    // var JSonData = '{"content":"I would like to recommend a new restaurant to you all", "messageType":"normal","contentType":"text"}';
+    // $.ajax({
+    //    url : link,
+    //    data : JSON.stringify(JSonData),
+    //    type : 'POST',
+    //    contentType : "application/json",
+    //    dataType : 'json',
+    //    success : function(Result) {
+    //     console.log('success');
+    //    },
+    //    beforeSend: function (xhr) {
+    //       xhr.setRequestHeader('X-mmx-app-id', 'gudif1h0wj3');
+    //       xhr.setRequestHeader('X-mmx-api-key', '69a7082d-b1f6-4c28-8f5d-2412da584590');
+    //    },
+    //    error: function (RcvData, error) {
+    //       console.log(RcvData);
+    //    }
+  }
+
+  $scope.removeUser = function(name) {
+    $scope.reservationHandler.removeReservation(name);
+  }
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
