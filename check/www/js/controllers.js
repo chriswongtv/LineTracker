8df28b89e1151c9b5555f26e3b67ff961d0664a4
Chris Wong <chris@cogifire.com>
2015-09-26 19:25:53 -0700

    Geolocation

--- check/www/js/controllers.js
+++ check/www/js/controllers.js
@@ -1,5 +1,39 @@
 angular.module('starter.controllers', [])
 
+.controller('RestaurantsCtrl', function($scope, $cordovaGeolocation) {
+  $scope.listing = [];
+  var listNum = 0;
+  var posOptions = {timeout: 10000, enableHighAccuracy: false};
+  var location = $cordovaGeolocation.getCurrentPosition(posOptions);
+  location.then(function (position) {
+      var latitude  = position.coords.latitude;
+      var longitude = position.coords.longitude;
+
+      console.log(latitude);
+      console.log(longitude);
+
+      // JSON.parse('https://api.soleo.com/businesses?Category=Restaurants&Latitude=' + latitude + '&Longitude=' + longitude + '&Radius=10&APIKey=hnqee6js7rekm8txj7p6fqbw', function(k, v) {
+      //   console.log("test " + k);
+      //   return v;
+      // });
+
+      var url = 'https://api.soleo.com/businesses?Category=Restaurants&Latitude=' + latitude + '&Longitude=' + longitude + '&Radius=10&APIKey=hnqee6js7rekm8txj7p6fqbw';
+
+      $.getJSON(url,function(data) { dataResponse(data.businesses); });
+    }, function(err) {
+      // error
+    });
+  //function dataResponse(data) { listing = data; console.log(listing); };
+  function dataResponse(data) {
+    for (i = 0; i < data.length; i++) {
+      $scope.listing[i] = data[i];
+      console.log($scope.listing[i]);
+      console.log(data[i]);
+      listNum++;
+    }
+  };
+})
+
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
