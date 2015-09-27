c62872afff3980f83fa8d2e05b0b191018134e42
Can Yildirim <canpy30@gmail.com>
2015-09-27 11:58:44 -0700

    submit button function adds reservation to the array

--- reg/www/js/controllers.js
+++ reg/www/js/controllers.js
@@ -1,11 +1,78 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
-  $scope.addCount = function() {
+  var reservationHandler = new WaitingListHandler();
+
+
+  $scope.addCount = function(inPartyName, inPartySize) {
     var url = 'https://prod-mmx-001.magnet.com:5221/mmxmgmt/api/v1/topics';
     var currentName;
+
+    reservationHandler.addReservation(new Reservation(inPartyName, inPartySize));
   };
   
+  <!-- class -->
+  function Reservation(inName, inPartySize) {
+    <!-- variables -->
+    this.username = inName;
+    this.partySize = inPartySize;
+    this.waitTime = 0;
+    this.posInQueue = 0;
+  }
+    
+
+    <!-- constructor -->
+    Reservation.prototype = {
+      constructor: Reservation,
+      //this.username = inName;
+      //this.partySize = inPartySize;
+      //console.log("constructor called");
+    };
+    <!-- functions -->
+    Reservation.prototype.getWaitTime = function (){
+      calculateWaitTime();
+      return this.waitTime;
+    };
+
+    Reservation.prototype.setPosInQueue = function (inPosInQueue){
+      this.posInQueue = inPosInQueue;
+    };
+
+    Reservation.prototype.calculateWaitTime = function () {
+      this.waitTime = (this.posInQueue)*10;
+    };
+  
+    <!-- class -->
+    function WaitingListHandler(){}
+      WaitingListHandler.prototype.waitingListQueue = [];
+      WaitingListHandler.prototype.waitingListSearchKeys = [];
+      WaitingListHandler.prototype.numOfReservations = 0;
+
+      WaitingListHandler.prototype.updateQueuePositions = function (){
+        for(var i = 0; i < this.numOfReservations; i++){
+          this.waitingListQueue[i].setPosInQueue(i+1);
+          this.waitingListQueue[i].calculateWaitTime();
+        }
+      };
+      WaitingListHandler.prototype.removeReservation = function (name){
+        var targetIndex = this.waitingListSearchKeys.indexOf(name);
+        var target = this.waitingListQueue[targetIndex];
+
+        <!-- remove item from both arrays -->
+        this.waitingListQueue.splice(targetIndex, 1);
+        this.waitingListSearchKeys.splice(targetIndex, 1);
+
+        <!-- decrement the number of reservations -->
+        this.numOfReservations--;
+        this.updateQueuePositions();
+        return target;
+      };
+      WaitingListHandler.prototype.addReservation = function (reservation){
+        this.waitingListQueue.push(reservation);
+        this.waitingListSearchKeys.push(reservation.username);
+        this.numOfReservations++;
+        this.updateQueuePositions();
+      };
   // $.post(url, data, function(response) {
   //   console.log('POST request success');
   // }, 'json');
