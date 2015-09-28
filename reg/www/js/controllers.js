928680fc74a4934cabe61e8fdd02100c953c9920
Can Yildirim <canpy30@gmail.com>
2015-09-27 18:10:31 -0700

    netcodeOPS toolkit

--- reg/www/js/controllers.js
+++ reg/www/js/controllers.js
@@ -1,7 +1,75 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $http) {
   var reservationHandler = new WaitingListHandler();
+
+
+ function netcodeOPS(){}
+
+netcodeOPS.prototype.sendMessage = function (appID, apiKey, contentType, channelName, message){
+
+  var req = 
+  {
+      method: 'POST',
+      url: ('https://prod-mmx-001.magnet.com:5221/mmxmgmt/api/v1/topics/' + channelName + '/publish'),
+      data: '{"content": {"message":" ' + message + ' ", "messageType":"normal", "contentType":"text" }}',
+      headers: {
+      'X-mmx-app-id': appID,
+      'X-mmx-api-key': apiKey,
+      'Content-Type': contentType
+    }
+  }
+
+  $http(req).
+  success(function(data, status, headers, config) 
+  {
+      console.log("success23124");
+  }).
+  error(function(data, status, headers, config) 
+  {
+      //error
+  });
+  // //The url we want is `www.nodejitsu.com:1337/`
+  // var options = {
+  //   protocol: 'https',
+  //   host: 'prod-mmx-001.magnet.com',
+  //   path: '/mmxmgmt/api/v1/topics/' + channelName + '/publish',
+  //   //since we are listening on a custom port, we need to specify it by hand
+  //   port: '5221',
+
+  //   headers: {
+  //     'X-mmx-app-id': '' + appID + '',
+  //     'X-mmx-api-key': '' + apiKey + '',
+  //     'Content-Type': '' + contentType + '',
+
+  //   },
+
+  //   //This is what changes the request to a POST request
+  //   method: 'POST'
+  // };
+
+  // callback = function(response) {
+  //   var str = ''
+  //   response.on('data', function (chunk) {
+  //     str += chunk;
+  //   });
+
+  //   response.on('end', function () {
+  //     console.log(str);//output
+  //   });
+  // }
+  // var req = http.request(options, callback);
+  // //This is the data we are posting, it needs to be a string or a buffer
+  // req.write( '{"content": {"message":" ' + message + ' ", "messageType":"normal", "contentType":"text" }}' );//********maybe same as data
+  // req.end();
+}
+
+var netOPS = new netcodeOPS();
+netOPS.sendMessage('gudif1h0wj3', 
+  '69a7082d-b1f6-4c28-8f5d-2412da584590', 
+  'application/json',
+  'testtopic4',
+  'welcome from nodeJS -CanY');
 
 
   $scope.addCount = function(inPartyName, inPartySize) {
